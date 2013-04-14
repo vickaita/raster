ae94a96ca0d6c68bebe046fd5b5faa00f1f15789
Vick Aita <vickaita@gmail.com>
2013-04-13 23:16:20 -0700

    Add pixel-groups.

--- src/vickaita/raster/geometry.cljs
+++ src/vickaita/raster/geometry.cljs
@@ -6,3 +6,11 @@
   (for [x (range (- origin-x radius) (+ origin-x (inc radius)))
         y (range (- origin-y radius) (+ origin-y (inc radius)))]
     [x y]))
+
+(defn pixel-groups
+  "Generate a list of lists of points surrounding each point between (0, 0) and
+  (w, h)."
+  [w h r]
+  (map (partial surround r)
+       (for [x (range w) y (range h)] [x y])))
+
