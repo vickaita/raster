8df4281852eb7fd093cb6642365d40009ca4d932
Vick Aita <vickaita@gmail.com>
2013-04-18 17:02:47 -0400

    Add convolution table.

--- src/vickaita/raster/geometry.cljs
+++ src/vickaita/raster/geometry.cljs
@@ -18,3 +18,17 @@
   [m]
   (map #(if (number? %) [% % % %] %) m))
 
+(defn convolution-table
+  "Assumes that matrix is square."
+  [m]
+  (let [radius (bit-shift-right (Math/sqrt (count m)) 1)
+        s (* -1 radius)
+        e (inc radius)
+        coords (for [y (range s e) x (range s e)] [x y])
+        matrix (map #(if (number? %) [% % % %] %) m)]
+    (into-array (flatten (remove #(= [0 0 0 0] (second %))
+                            (map #(vector %1 %2) coords matrix))))))
+
+(convolution-table [0 1 0
+                    1 1 1
+                    0 1 0])
