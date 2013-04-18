a2e3161f538c317dd562d5aa4d57ee06d8409598
Vick Aita <vickaita@gmail.com>
2013-04-18 17:53:30 -0400

    Add blur to filters.
    A basic blur to test the convolution function.
    
    Fix indentation of invert.

--- src/vickaita/raster/filters.cljs
+++ src/vickaita/raster/filters.cljs
@@ -1,9 +1,15 @@
 (ns vickaita.raster.filters
-  (:require-macros [vickaita.raster.macros :refer [dopixel]])
+  (:require-macros [vickaita.raster.macros :refer [dopixels]])
   (:require [vickaita.raster.core :as c :refer [image-data width height data]]
             [vickaita.raster.geometry :refer [surround]]))
 
 (defn invert
   [img]
   (dopixels [[r g b a] img]
-           [(- 255 r) (- 255 g) (- 255 b) a]))
+            [(- 255 r) (- 255 g) (- 255 b) a]))
+
+(defn blur
+  [img]
+  (c/convolute [0 1 0
+                1 1 1
+                0 1 0] 5 0 img))
