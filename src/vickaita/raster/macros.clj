416157cd943415f7031c5f8fd133794280f3b223
Vick Aita <vickaita@gmail.com>
2013-04-18 21:24:24 -0400

    Add let binding to the dopixels macro.

--- src/vickaita/raster/macros.clj
+++ src/vickaita/raster/macros.clj
@@ -9,7 +9,9 @@
 (defmacro dopixels
   [bindings body]
   (let [form (first bindings)
-        src-img (second bindings)]
+        src-img (second bindings)
+        has-let (and (> (count bindings) 2) (= :let (nth bindings 2)))
+        let-bindings (if has-let (nth bindings 3) [])]
     `(let [w# (vickaita.raster.core/width ~src-img)
            h# (vickaita.raster.core/height ~src-img)
            src-data# (vickaita.raster.core/data ~src-img)
@@ -20,7 +22,8 @@
          (let [~(nth form 0) (aget src-data# r#)
                ~(nth form 1) (aget src-data# g#)
                ~(nth form 2) (aget src-data# b#)
-               ~(nth form 3) (aget src-data# a#)]
+               ~(nth form 3) (aget src-data# a#)
+               ~@let-bindings]
            (aset dst-data# r# ~(nth body 0))
            (aset dst-data# g# ~(nth body 1))
            (aset dst-data# b# ~(nth body 2))
@@ -28,49 +31,3 @@
          (when (< a# n#)
            (recur (+ 4 r#) (+ 4 g#) (+ 4 b#) (+ 4 a#))))
        dst-img#)))
-
-(comment 
-
-  (defmacro dopixel
-    [[form src-img] & body]
-    (if (and (= 1 (count body)) (vec4? (first body)))
-      `(let [w# (vickaita.raster.core/width ~src-img)
-             h# (vickaita.raster.core/height ~src-img)
-             src-data# (vickaita.raster.core/data ~src-img)
-             dst-img# (vickaita.raster.core/image-data w# h#)
-             dst-data# (vickaita.raster.core/data dst-img#)
-             n# (dec (* 4 (count ~src-img)))]
-         (loop [r# 0 g# 1 b# 2 a# 3]
-           (let [~(nth form 0) (aget src-data# r#)
-                 ~(nth form 1) (aget src-data# g#)
-                 ~(nth form 2) (aget src-data# b#)
-                 ~(nth form 3) (aget src-data# a#)]
-             (aset dst-data# r# ~(nth (first body) 0))
-             (aset dst-data# g# ~(nth (first body) 1))
-             (aset dst-data# b# ~(nth (first body) 2))
-             (aset dst-data# a# ~(nth (first body) 3)))
-           (when (< a# n#)
-             (recur (+ 4 r#) (+ 4 g#) (+ 4 b#) (+ 4 a#))))
-         dst-img#)
-      `(let [w# (vickaita.raster.core/width ~src-img)
-             h# (vickaita.raster.core/height ~src-img)
-             src-data# (vickaita.raster.core/data ~src-img)
-             dst-img# (vickaita.raster.core/image-data w# h#)
-             dst-data# (vickaita.raster.core/data dst-img#)
-             n# (dec (* 4 (count ~src-img)))]
-         (loop [r# 0 g# 1 b# 2 a# 3]
-           (let [~(nth form 0) (aget src-data# r#)
-                 ~(nth form 1) (aget src-data# g#)
-                 ~(nth form 2) (aget src-data# b#)
-                 ~(nth form 3) (aget src-data# a#)
-                 [rv# gv# bv# av# (do ~@body)]]
-             (aset dst-data# r# rv#)
-             (aset dst-data# g# gv#)
-             (aset dst-data# b# bv#)
-             (aset dst-data# a# av#))
-           (when (< a# n#)
-             (recur (+ 4 r#) (+ 4 g#) (+ 4 b#) (+ 4 a#))))
-         dst-img#)
-      ))
-
-)
